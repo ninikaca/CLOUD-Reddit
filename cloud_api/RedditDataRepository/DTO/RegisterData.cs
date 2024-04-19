53474658b991b2f7da5da13933275b5681009e03
Katarina Marinković <ninikaca@gmail.com>
2024-04-19 16:59:29 +0200

    Create RegisterData.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/DTO/RegisterData.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedditDataRepository.DTO
+{
+    public class RegisterData
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [MinLength(1, ErrorMessage = "Name must have at least 1 character.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Surname is required.")]
+        [MinLength(1, ErrorMessage = "Surname must have at least 1 character.")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Address is required.")]
+        [MinLength(1, ErrorMessage = "Address must have at least 1 character.")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "City is required.")]
+        [MinLength(1, ErrorMessage = "City must have at least 1 character.")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Country is required.")]
+        [MinLength(1, ErrorMessage = "Country must have at least 1 character.")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required.")]
+        [MinLength(1, ErrorMessage = "Phone number must have at least 1 character.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [MinLength(1, ErrorMessage = "Email must have at least 1 character.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(1, ErrorMessage = "Password must have at least 1 character.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Image URL is required.")]
+        [MinLength(1, ErrorMessage = "Image URL must have at least 1 character.")]
+        public string Image { get; set; }
+    }
+}
