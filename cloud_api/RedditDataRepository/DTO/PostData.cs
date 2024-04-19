456fa5922b423fd2857bb955ae96b12fdb9fbfe7
Katarina Marinković <ninikaca@gmail.com>
2024-04-19 16:59:26 +0200

    Create PostData.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/DTO/PostData.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedditDataRepository.DTO
+{
+    public class PostData
+    {
+        [Required]
+        [MinLength(1)]
+        public string Title { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Content { get; set; }
+
+        public string Image { get; set; }
+    }
+}
