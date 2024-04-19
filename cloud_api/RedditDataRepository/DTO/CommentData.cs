8ea815972e031860ed0b65aa3a20d75521966de1
Katarina Marinković <ninikaca@gmail.com>
2024-04-19 16:59:34 +0200

    Create CommentData.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/DTO/CommentData.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedditDataRepository.DTO
+{
+    public class CommentData
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string PostId { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        public string Content { get; set; }
+    }
+}
