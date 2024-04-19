f32d88d8eb1b72a4cf8e0456a2a4ed3a9a310204
Katarina Marinković <ninikaca@gmail.com>
2024-04-19 16:59:41 +0200

    Create RetrivedPostsData.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/DTO/RetrivedPostsData.cs
@@ -0,0 +1,30 @@
+﻿using RedditDataRepository.Models;
+using System.Collections.Generic;
+
+namespace RedditDataRepository.DTO
+{
+    public class RetrivedPostsData
+    {
+        public string PostId { get; set; }
+
+        public string PostTitle { get; set; }
+
+        public string PostContent { get; set; }
+
+        public string PostAuthor { get; set; }
+
+        public string PostImageUrl { get; set; } = "";
+
+        public int VotesCount { get; set; } = 0;
+
+        public int CommentsCount { get; set; } = 0;
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+
+        public List<string> UsersUpvoted { get; set; } = new List<string>();
+
+        public List<string> UsersDownvoted { get; set; } = new List<string>();
+
+        public List<string> UsersSubscribed { get; set; } = new List<string>();
+    }
+}
