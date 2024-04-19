df7317ba9243255b7e95c5243ac14713f3d7af3d
Katarina Marinković <ninikaca@gmail.com>
2024-04-19 17:00:21 +0200

    Create PostQueryData.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/DTO/PostQueryData.cs
@@ -0,0 +1,15 @@
+﻿namespace RedditDataRepository.DTO
+{
+    public class PostQueryData
+    {
+        public int Page { get; set; } = 1;
+
+        public int PageSize { get; set; } = 5;
+
+        public string Sort { get; set; } = "";
+
+        public string SearchQuery { get; set; } = "";
+
+        public string UserId { get; set; } = "";
+    }
+}
