5f1d44d5440a657c37942e92b9f8b4e0abdb8d01
Katarina Marinković <ninikaca@gmail.com>
2024-04-20 16:58:54 +0200

    Create CreateToken.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/Account/CreateToken.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RedditDataRepository.Account
+{
+    public class CreateToken
+    {
+        public static readonly string _secretKey = "NINIKACAGMAILKOMNINININIKACAKACAKACAKACA";
+
+        public static string Token(string email, string user_id)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(
+                    new[]
+                    {
+                        new Claim(ClaimTypes.Email, email),
+                        new Claim("Id", user_id)
+                    }
+                ),
+                Expires = DateTime.UtcNow.AddHours(2),
+                Issuer = "ninikaca",
+                Audience = "kac",
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
