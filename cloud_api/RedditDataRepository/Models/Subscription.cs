1039ddb233c9005cbe6e6e74e02125792d4409d7
Katarina Marinković <ninikaca@gmail.com>
2024-04-22 17:00:56 +0200

    Create Subscription.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/Models/Subscription.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace RedditDataRepository.Models
+{
+    public class Subscription : TableEntity
+    {
+        public string PostId { get; set; }
+
+        public string UserId { get; set; }
+
+        public Subscription()
+        {
+            RowKey = Guid.NewGuid().ToString();
+            PartitionKey = "Subscription";
+        }
+
+        public Subscription(string postId, string userId)
+        {
+            RowKey = Guid.NewGuid().ToString();
+            PartitionKey = "Subscription";
+
+            PostId = postId;
+            UserId = userId;
+        }
+    }
+}
