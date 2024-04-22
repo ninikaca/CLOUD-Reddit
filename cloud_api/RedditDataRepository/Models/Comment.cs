7f96b4f7e949359040634ab11618cfc6912581e8
Katarina Marinković <ninikaca@gmail.com>
2024-04-22 17:00:50 +0200

    Create Comment.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/Models/Comment.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace RedditDataRepository.Models
+{
+    public class Comment : TableEntity
+    {
+        public string PostId { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Content { get; set; }
+
+        public Comment()
+        {
+            RowKey = Guid.NewGuid().ToString();
+            PartitionKey = "Comment";
+        }
+
+        public Comment(string postId, string userId, string content)
+        {
+            RowKey = Guid.NewGuid().ToString();
+            PartitionKey = "Comment";
+
+            PostId = postId;
+            UserId = userId;
+            Content = content;
+        }
+    }
+}
