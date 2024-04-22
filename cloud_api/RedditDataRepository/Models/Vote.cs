57161d9fa4df7ff56e4895eed39a6644c675d186
Katarina Marinković <ninikaca@gmail.com>
2024-04-22 17:00:44 +0200

    Create Vote.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/Models/Vote.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace RedditDataRepository.Models
+{
+    public class Vote : TableEntity
+    {
+        public string UserId { get; set; }
+
+        public string PostId { get; set; }
+
+        public bool IsUpvoted { get; set; }
+
+        public Vote()
+        {
+            PartitionKey = "Vote";
+            RowKey = Guid.NewGuid().ToString();
+        }
+
+        public Vote(string userId, string postId, bool isUpvoted)
+        {
+            PartitionKey = "Vote";
+            RowKey = Guid.NewGuid().ToString();
+
+            UserId = userId;
+            PostId = postId;
+            IsUpvoted = isUpvoted;
+        }
+    }
+}
