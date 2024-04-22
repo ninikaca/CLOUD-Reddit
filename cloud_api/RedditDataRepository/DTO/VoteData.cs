ed56aadc08d60fa97dfcb0d1c36abe8744565013
Katarina Marinković <ninikaca@gmail.com>
2024-04-22 17:00:32 +0200

    Create VoteData.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/DTO/VoteData.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RedditDataRepository.DTO
+{
+    public class VoteData
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        public string PostId { get; set; }
+
+        [Required]
+        public bool IsUpvoted { get; set; }
+    }
+}
