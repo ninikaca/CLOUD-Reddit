d291262fac4f3deed3b5ac84d0d771b2f7142e5d
Katarina Marinković <ninikaca@gmail.com>
2024-04-22 17:00:39 +0200

    Create User.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/Models/User.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using RedditDataRepository.DTO;
+using System;
+
+namespace RedditDataRepository.Models
+{
+    public class User : TableEntity
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public User()
+        {
+            PartitionKey = "User";
+            Id = Guid.NewGuid().ToString();
+        }
+
+        public User(string name, string surname, string address, string city, string country, string phone, string email, string password, string imageBlobUrl)
+        {
+            PartitionKey = "User";
+            RowKey = email;
+
+            Id = Guid.NewGuid().ToString();
+            Name = name;
+            Surname = surname;
+            Address = address;
+            City = city;
+            Country = country;
+            Phone = phone;
+            Email = email;
+            Password = password;
+            ImageUrl = imageBlobUrl;
+        }
+
+        public User(RegisterData data)
+        {
+            PartitionKey = "User";
+            RowKey = data.Email;
+
+            Id = Guid.NewGuid().ToString();
+            Name = data.Name;
+            Surname = data.Surname;
+            Address = data.Address;
+            City = data.City;
+            Country = data.Country;
+            Phone = data.Phone;
+            Email = data.Email;
+            Password = data.Password;
+            ImageUrl = data.Image;
+        }
+
+        public User(UpdateData data)
+        {
+            PartitionKey = "User";
+            RowKey = data.Email;
+
+            Id = Guid.NewGuid().ToString();
+            Name = data.Name;
+            Surname = data.Surname;
+            Address = data.Address;
+            City = data.City;
+            Country = data.Country;
+            Phone = data.Phone;
+            Email = data.Email;
+            Password = data.Password;
+            ImageUrl = data.Image;
+        }
+    }
+}
