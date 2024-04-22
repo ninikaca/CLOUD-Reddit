6cc49980035cb57378a9358f631f97eac477a198
Katarina Marinković <ninikaca@gmail.com>
2024-04-22 17:01:00 +0200

    Create Post.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/Models/Post.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using RedditDataRepository.DTO;
+using System;
+
+namespace RedditDataRepository.Models
+{
+    public class Post : TableEntity
+    {
+        public string Title { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Content { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public Post()
+        {
+            long timestamp = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+
+            PartitionKey = "Post";
+            RowKey = timestamp.ToString();
+        }
+
+        public Post(string title, string userId, string content, string imageBlobUrl)
+        {
+            long timestamp = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+
+            PartitionKey = "Post";
+            RowKey = timestamp.ToString();
+
+            Title = title;
+            UserId = userId;
+            Content = content;
+            ImageUrl = imageBlobUrl;
+        }
+
+        public Post(PostData data)
+        {
+            long timestamp = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+
+            PartitionKey = "Post";
+            RowKey = timestamp.ToString();
+
+            Title = data.Title;
+            UserId = data.UserId;
+            Content = data.Content;
+            ImageUrl = data.Image;
+        }
+    }
+}
