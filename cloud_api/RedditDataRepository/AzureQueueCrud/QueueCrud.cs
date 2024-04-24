b7f831df89b97d67d5db14633289bff1c96da713
Katarina Marinković <ninikaca@gmail.com>
2024-04-24 17:01:24 +0200

    Create QueueCrud.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditDataRepository/AzureQueueCrud/QueueCrud.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.WindowsAzure.Storage.Queue;
+using System;
+using System.Threading.Tasks;
+
+namespace RedditDataRepository.AzureQueueCrud
+{
+    public class QueueCrud
+    {
+        public static async Task<bool> AddToQueue(CloudQueue queue, string content)
+        {
+            try
+            {
+                CloudQueueMessage queueMessage = new CloudQueueMessage(content);
+                await queue.AddMessageAsync(queueMessage);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static async Task<CloudQueueMessage> ReadMessage(CloudQueue queue)
+        {
+            try
+            {
+                CloudQueueMessage message = await queue.GetMessageAsync();
+                return message;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static async Task<bool> DeleteMessage(CloudQueue queue, CloudQueueMessage messageId)
+        {
+            try
+            {
+                await queue.DeleteMessageAsync(messageId);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
