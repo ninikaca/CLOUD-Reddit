e34de7bc9d08779a5316aec29fc6162b12e66254
Katarina Marinković <ninikaca@gmail.com>
2024-04-25 11:43:47 +0200

    init service
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/HealthStatusService/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Optimization;
+
+namespace HealthStatusService
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/Site.css"));
+        }
+    }
+}
