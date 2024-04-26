de111bab8fffff8f1d86b7d5b42577d30eb0885e
Katarina Marinković <ninikaca@gmail.com>
2024-04-26 11:44:30 +0200

    Create HealthStatusesCrud.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/HealthStatusService/AzureTableCrud/HealthStatusesCrud.cs
@@ -0,0 +1,39 @@
+﻿using HealthStatusService.Models;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HealthStatusService.AzureTableCrud
+{
+    public class HealthStatusesCrud
+    {
+        public static async Task<List<HealthStatus>> ReadStatuses(CloudTable table)
+        {
+            if (table == null)
+                return null;
+
+            TableQuery<HealthStatus> query = new TableQuery<HealthStatus>();
+            TableContinuationToken continuationToken = null;
+            var statuses = new List<HealthStatus>();
+
+            do
+            {
+                TableQuerySegment<HealthStatus> queryResult = await table.ExecuteQuerySegmentedAsync(query, continuationToken);
+                statuses.AddRange(queryResult.Results);
+                continuationToken = queryResult.ContinuationToken;
+            } while (continuationToken != null);
+
+            return statuses.Take(500).ToList();
+        }
+
+        public static async Task<HealthStatus> InsertStatus(CloudTable table, HealthStatus status)
+        {
+            if (table == null || status == null)
+                return null;
+
+            TableOperation insertOperation = TableOperation.InsertOrMerge(status);
+            return (await table.ExecuteAsync(insertOperation)).Result as HealthStatus;
+        }
+    }
+}
