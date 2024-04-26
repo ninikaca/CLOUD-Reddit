615051329f82f24400e132aff708a93e472f3f63
Katarina Marinković <ninikaca@gmail.com>
2024-04-26 11:44:25 +0200

    Create HealthCheck.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/HealthStatusService/Models/HealthCheck.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace HealthStatusService.Models
+{
+    public class HealthStatus : TableEntity
+    {
+        public bool IsRedditAvailable { get; set; }
+
+        public bool IsNotificationAvailable { get; set; }
+
+        public HealthStatus()
+        {
+            RowKey = Guid.NewGuid().ToString();
+            PartitionKey = "HealthStatus";
+        }
+
+        public HealthStatus(bool reddit, bool notification)
+        {
+            RowKey = Guid.NewGuid().ToString();
+            PartitionKey = "HealthStatus";
+            IsRedditAvailable = reddit;
+            IsNotificationAvailable = notification;
+        }
+    }
+}
