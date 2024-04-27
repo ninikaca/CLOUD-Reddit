9d806aa109b05a1e47907dcbdb10fdcbf9d7162d
Katarina Marinković <ninikaca@gmail.com>
2024-04-27 16:45:22 +0200

    Create HealthStatusShowController.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/HealthStatusService/Controllers/HealthStatusShowController.cs
@@ -0,0 +1,31 @@
+﻿using HealthStatusService.HealthCheck;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace HealthStatusService.Controllers
+{
+    public class HealthStatusShowController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            (double uptime, List<bool> stats) = await RetreiveHealthCheckData.RetreiveData();
+            ViewBag.uptime = uptime;
+            ViewBag.uptimePercentage = Math.Round(uptime, 0).ToString() + "%";
+            ViewBag.uptimeClass = GetUptimeClass(uptime);
+            ViewBag.stats = stats;
+            return View();
+        }
+
+        private string GetUptimeClass(double uptime)
+        {
+            if (uptime > 90)
+                return "bg-success text-success";
+            else if (uptime >= 80)
+                return "bg-warning text-warning";
+            else
+                return "bg-danger text-danger";
+        }
+    }
+}
