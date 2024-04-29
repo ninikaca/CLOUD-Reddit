e9a10eac591b59e176eac22590b03d251d1656da
Katarina Marinković <ninikaca@gmail.com>
2024-04-29 16:49:07 +0200

    Create Administrator.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/NotificationService/Models/Administrator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace NotificationService.Models
+{
+    public class Administrator : TableEntity
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public Administrator()
+        {
+            PartitionKey = "Administrator";
+            Id = Guid.NewGuid().ToString().Replace("-", "");
+        }
+
+        public Administrator(string name, string email, string password)
+        {
+            PartitionKey = "Administrator";
+            RowKey = email;
+            Id = Guid.NewGuid().ToString().Replace("-", "");
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
