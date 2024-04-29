e175c13139e0cb2f799a68af417cc709527fec5f
Katarina Marinković <ninikaca@gmail.com>
2024-04-29 16:49:04 +0200

    Create Notification.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/NotificationService/Models/Notification.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace NotificationService.Models
+{
+    public class Notification : TableEntity
+    {
+        public string CommentId { get; set; }
+
+        public int EmailsSent { get; set; } = 0;
+
+        public Notification()
+        {
+            RowKey = Guid.NewGuid().ToString();
+            PartitionKey = "Notification";
+        }
+
+        public Notification(string id, int emailsSent)
+        {
+            RowKey = Guid.NewGuid().ToString();
+            PartitionKey = "Comment";
+            CommentId = id;
+            EmailsSent = emailsSent;
+        }
+    }
+}
