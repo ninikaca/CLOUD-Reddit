9f4c1617563815332bb83871a822005c50e388e5
Katarina Marinković <ninikaca@gmail.com>
2024-04-30 16:49:33 +0200

    Create Queue
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/NotificationService/Queue/CommentsQueue.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using NotificationService.AzureTableCrud;
+using NotificationService.Models;
+using RedditDataRepository.Account;
+using RedditDataRepository.AzureQueueCrud;
+using RedditDataRepository.AzureTableCrud;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NotificationService.Queue
+{
+    public class CommentsQueue
+    {
+        private readonly static CloudQueue CommentNotificationsQueue = Account.GetQueue("notifications");
+        private readonly static CloudTable commentsTable = Account.GetTable("comments");
+        private readonly static CloudTable usersTable = Account.GetTable("users");
+        private readonly static CloudTable subsTable = Account.GetTable("subscriptions");
+        private readonly static CloudTable notificationsTable = Account.GetTable("notifications");
+
+        public static async Task IsCommentAvailableForSend()
+        {
+            var message = await QueueCrud.ReadMessage(CommentNotificationsQueue);
+            if (message == null)
+                return;
+
+            var comment = await CommentsCrud.ReadComment(commentsTable, message.AsString);
+            if (comment == null) return;
+
+            var subs = await SubscriptionCrud.ReadSubscriptionsForPost(subsTable, comment.PostId);
+
+            var emails = new List<string>();
+            foreach (var sub in subs)
+            {
+                var user = await UsersCrud.ReadUserById(usersTable, sub.UserId);
+                emails.Add(user.Email);
+            }
+
+            // Send all emails
+            int sent = 0;
+            foreach (var email in emails)
+            {
+                await Mailer.Sender.SendEmail(comment.Content, email, "New comment has been added on post");
+                sent++;
+            }
+
+            // Save notification info how much emails has been sent
+            Notification notification = new Notification(comment.RowKey, sent);
+            await NotificationsCrud.InsertNotification(notificationsTable, notification);
+
+            // Remove from queue
+            await QueueCrud.DeleteMessage(CommentNotificationsQueue, message);
+        }
+    }
+}
---
+++ cloud_api/NotificationService/Queue/ServiceQueue.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using NotificationService.AzureTableCrud;
+using RedditDataRepository.Account;
+using RedditDataRepository.AzureQueueCrud;
+using System.Threading.Tasks;
+
+namespace NotificationService.Queue
+{
+    public class ServiceQueue
+    {
+        private readonly static CloudQueue AdminNotificationQueue = Account.GetQueue("AdminNotificationQueue");
+        private readonly static CloudTable adminsTable = Account.GetTable("administrators");
+
+        public static async Task IsServiceOffline()
+        {
+            var message = await QueueCrud.ReadMessage(AdminNotificationQueue);
+            if (message == null)
+                return;
+
+            try
+            {
+                if (message == null) return;
+
+                // Send email for each administrator
+                foreach (var administrator in await AdministratorsCrud.ReadAdministrators(adminsTable))
+                {
+                    await Mailer.Sender.SendEmail(message.AsString, administrator.Email, "Service outage information");
+                }
+
+                // Remove from admin queue
+                await QueueCrud.DeleteMessage(AdminNotificationQueue, message);
+            }
+            catch
+            {
+                return;
+            }
+        }
+    }
+}
