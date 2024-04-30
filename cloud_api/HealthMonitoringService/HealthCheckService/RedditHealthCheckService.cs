23ce874d4c04a4528df6e200d07a325258e83752
Katarina Marinković <ninikaca@gmail.com>
2024-04-30 11:42:21 +0200

    Create RedditHealthCheckService.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/HealthMonitoringService/HealthCheckService/RedditHealthCheckService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace HealthMonitoringService.HealthCheckService
+{
+    public class RedditHealthCheckService
+    {
+        public static async Task<bool> RunCheck()
+        {
+            bool is_online = false;
+
+            try
+            {
+                var endpoint = RoleEnvironment.Roles["RedditServiceWebRole"].Instances.First().InstanceEndpoints["health-monitoring-r"];
+                string ipAddress = endpoint.IPEndpoint.Address.ToString();
+                int port = endpoint.IPEndpoint.Port;
+
+                using (var client = new TcpClient(ipAddress, port))
+                {
+                    using (var reader = new StreamReader(client.GetStream()))
+                    {
+                        string response = await reader.ReadLineAsync();
+                        is_online = true;
+                    }
+                }
+            }
+            catch
+            {
+                is_online = false;
+            }
+
+            return is_online;
+        }
+    }
+}
