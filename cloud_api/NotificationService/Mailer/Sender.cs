fd88f59810c13e09ba9483e1034a8a2af1e34791
Katarina Marinković <ninikaca@gmail.com>
2024-04-30 16:49:40 +0200

    Create Sender.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/NotificationService/Mailer/Sender.cs
@@ -0,0 +1,23 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace NotificationService.Mailer
+{
+    public class Sender
+    {
+        public static async Task<bool> SendEmail(string message, string to, string subject)
+        {
+            var apiKey = "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";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("ninikaca@gmail.com", "cloud");
+            var toEmail = new EmailAddress(to, to.Split('@')[0]);
+            var plainTextContent = message;
+            var htmlContent = "<strong>" + message + "</strong>";
+            var msg = MailHelper.CreateSingleEmail(from, toEmail, subject, plainTextContent, htmlContent);
+            var response = await client.SendEmailAsync(msg);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
