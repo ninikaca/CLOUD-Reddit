2ae593041f698c16a2ef0c76dbe42d7434857614
Katarina Marinković <ninikaca@gmail.com>
2024-05-07 18:40:46 +0200

    Create Admin.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/AdminToolsConsoleApp/Models/Admin.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace AdminToolsConsoleApp.Models
+{
+    public class Admin : TableEntity
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public Admin()
+        {
+            PartitionKey = "Admin";
+            Id = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+        }
+
+        public Admin(string name, string email, string password)
+        {
+            PartitionKey = "Admin";
+            RowKey = email;
+            Id = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeSeconds();
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return $"`````````````````````````````````````````````````````\n" +
+                   $"ID       : {Id}\n" +
+                   $"Name     : {Name}\n" +
+                   $"Email    : {Email}\n" +
+                   $"Password : {new string('*', Password.Length)}\n" +
+                   "`````````````````````````````````````````````````````\n";
+        }
+    }
+}
