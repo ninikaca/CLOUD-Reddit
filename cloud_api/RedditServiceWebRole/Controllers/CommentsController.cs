13af0b445f6890f72b7788a4955ea88c2f0e847c
Katarina Marinković <ninikaca@gmail.com>
2024-05-26 17:03:43 +0200

    Create CommentsController.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditServiceWebRole/Controllers/CommentsController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.WindowsAzure.Storage.Queue;
+using Microsoft.WindowsAzure.Storage.Table;
+using RedditDataRepository.Account;
+using RedditDataRepository.AzureQueueCrud;
+using RedditDataRepository.AzureTableCrud;
+using RedditDataRepository.DTO;
+using RedditDataRepository.Models;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RedditServiceWebRole.Controllers
+{
+    [RoutePrefix("api/comment")]
+    public class CommentsController : ApiController
+    {
+        private readonly CloudTable comments = Account.GetTable("comments");
+        private readonly CloudQueue notifications = Account.GetQueue("notifications");
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateToken]
+        public async Task<IHttpActionResult> Insert(CommentData data)
+        {
+            try
+            {
+                if (ModelState.IsValid == false)
+                    return BadRequest();
+
+                Comment comment = await CommentsCrud.InsertComment(comments, new Comment(data.PostId, data.UserId, data.Content));
+                if (comment != null && await QueueCrud.AddToQueue(notifications, comment.RowKey)) // Add to cloud queue
+                {
+                    return Ok(comment);
+                }
+                else
+                    return BadRequest();
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}")]
+        [ValidateToken]
+        public async Task<IHttpActionResult> Delete(string id)
+        {
+            try
+            {
+                if (ModelState.IsValid == false)
+                    return BadRequest();
+
+                if (await CommentsCrud.DeleteComment(comments, id))
+                    return Ok();
+                else
+                    return BadRequest();
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
