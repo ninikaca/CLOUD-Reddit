7d56d151c1dd3c90f8c883855bd07b175a59eeb5
Katarina Marinković <ninikaca@gmail.com>
2024-05-26 17:04:16 +0200

    Create RouteConfig.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditServiceWebRole/App_Start/RouteConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace RedditServiceWebRole
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "api/{controller}/{action}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
