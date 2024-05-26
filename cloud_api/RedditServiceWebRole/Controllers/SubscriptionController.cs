876b4987a8d463593aeaac5026ca686f7b796069
Katarina Marinković <ninikaca@gmail.com>
2024-05-26 17:03:46 +0200

    Create SubscriptionController.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditServiceWebRole/Controllers/SubscriptionController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using RedditDataRepository.Account;
+using RedditDataRepository.AzureTableCrud;
+using RedditDataRepository.DTO;
+using RedditDataRepository.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RedditServiceWebRole.Controllers
+{
+    [RoutePrefix("api/subscriptions")]
+    public class SubscriptionController : ApiController
+    {
+        private readonly CloudTable subscriptions = Account.GetTable("subscriptions");
+
+        [HttpPost]
+        [Route("subscribe")]
+        [ValidateToken]
+        public async Task<IHttpActionResult> Subscribe(SubscriptionData data)
+        {
+            try
+            {
+                List<Subscription> subs = await SubscriptionCrud.ReadSubscriptionsForPost(subscriptions, data.PostId);
+
+                if (subs.FirstOrDefault(s => s.UserId == data.UserId) != null)
+                    return BadRequest();
+
+                if ((await SubscriptionCrud.InsertSubscription(subscriptions, new Subscription(data.PostId, data.UserId))) != null)
+                    return Ok();
+                else
+                    return BadRequest();
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
