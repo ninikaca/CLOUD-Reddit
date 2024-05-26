090ab196f568d39912e55893d4fea151ac9ef930
Katarina Marinković <ninikaca@gmail.com>
2024-05-26 17:03:37 +0200

    Create UsersController.cs
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ cloud_api/RedditServiceWebRole/Controllers/UsersController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using RedditDataRepository.Account;
+using RedditDataRepository.AzureBlobCrud;
+using RedditDataRepository.AzureTableCrud;
+using RedditDataRepository.DTO;
+using RedditDataRepository.Models;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RedditServiceWebRole.Controllers
+{
+    [RoutePrefix("api/users")]
+    public class UsersController : ApiController
+    {
+        private readonly CloudTable users = Account.GetTable("users");
+
+        [HttpGet]
+        [Route("get/{id}")]
+        [ValidateToken]
+        public async Task<IHttpActionResult> Get(string id)
+        {
+            try
+            {
+                User user = await UsersCrud.ReadUserById(users, id);
+
+                if (user != null)
+                    return Ok(user);
+                else
+                    return NotFound();
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        }
+
+        [HttpPatch]
+        [Route("update")]
+        [ValidateToken]
+        public async Task<IHttpActionResult> Update(UpdateData data)
+        {
+            try
+            {
+                User user = await UsersCrud.ReadUserById(users, data.Id);
+
+                if (user != null)
+                {
+                    // Check if email changed if changed check is unique
+                    if(user.Email != data.Email)
+                    {
+                        // Check is email already in use
+                        if ((await UsersCrud.ReadUser(users, data.Email) != null))
+                            return BadRequest();
+                    }
+
+                    // If new profile photo provided, replace the old one
+                    if (data.Image != null && data.Image != user.ImageUrl)
+                    {
+                        data.Image = await ImagesCrud.UploadImage(data.Image);
+
+                        // Image failed to upload
+                        if (data.Image == "")
+                            return BadRequest();
+                    }
+                    else
+                    {
+                        // Keep old profile image if new one not imported
+                        data.Image = user.ImageUrl;
+                    }
+
+                    User toUpdate = new User(data);
+                    toUpdate.Id = user.Id; // Keep old id
+                    user = await UsersCrud.UpdateUser(users, user.RowKey, toUpdate);
+
+                    if (user != null)
+                        return Ok(CreateToken.Token(user.Email, user.Id));
+                    else
+                        return BadRequest();
+
+                }
+                else
+                    return NotFound();
+            }
+            catch
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
