fe23ee5b217c61949f3c0422ee3e2c6a3a21d133
Katarina Marinković <ninikaca@gmail.com>
2024-06-01 18:39:04 +0200

    comment service
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ frontend/src/services/vote/UnvotePostService.tsx
@@ -0,0 +1,33 @@
+import axios, { AxiosResponse } from "axios";
+import { API_URL } from "../../main";
+import { VoteData } from "../../models/vote/VoteData";
+
+const UnvotePostService = async (
+  token: string,
+  data: VoteData
+): Promise<boolean> => {
+  try {
+    if (!token || token === "") return false;
+
+    const response: AxiosResponse<boolean> = await axios.post(
+      API_URL + `votes/remove`,
+      data,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch {
+    return false;
+  }
+};
+
+export default UnvotePostService;
