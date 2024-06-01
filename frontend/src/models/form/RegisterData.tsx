e811f50902cfd69febbe5cdab9429872f70bb642
Katarina Marinković <ninikaca@gmail.com>
2024-06-01 18:32:05 +0200

    prepare form
    Co-Authored-By: Valentina Radivojević <116080215+tinar24@users.noreply.github.com>
    Co-Authored-By: nevena <104759106+nnevennica@users.noreply.github.com>
    Co-Authored-By: Sara Simovic  <116080339+saarraass@users.noreply.github.com>

---
+++ frontend/src/models/form/RegisterData.tsx
@@ -0,0 +1,13 @@
+interface RegisterData {
+  name: string;
+  surname: string;
+  address: string;
+  city: string;
+  country: string;
+  phone: string;
+  email: string;
+  password: string;
+  image: string;
+}
+
+export default RegisterData;
